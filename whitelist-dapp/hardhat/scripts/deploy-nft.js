bfabb479e7b9965d5d3627e9563a17bfea3b42c8
Wesley Coetzee <coetzeewr@gmail.com>
2023-07-12 08:04:15 +0200

    feat: Add deploy NFT

---
+++ whitelist-dapp/hardhat/scripts/deploy-nft.js
@@ -0,0 +1,37 @@
+const hre = require("hardhat");
+
+const contractAddress = "0xC61B03A7d608Dc3c289280A981c9615f1b3E6C4B";
+
+async function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function main() {
+  // Deploy the CryptoDevs Contract
+  const nftContract = await hre.ethers.deployContract("CryptoDevs", [
+    contractAddress,
+  ]);
+
+  // wait for the contract to deploy
+  await nftContract.waitForDeployment();
+
+  // print the address of the deployed contract
+  console.log("NFT Contract Address:", nftContract.target);
+
+  // Sleep for 30 seconds while Etherscan indexes the new contract deployment
+  await sleep(30 * 1000); // 30s = 30 * 1000 milliseconds
+
+  // Verify the contract on etherscan
+  await hre.run("verify:verify", {
+    address: nftContract.target,
+    constructorArguments: [contractAddress],
+  });
+}
+
+// Call the main function and catch if there is any error
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
